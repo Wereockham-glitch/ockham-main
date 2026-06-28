66a7201d0c8449bb7c522c1d0567bb94e9e72ef9
Lucas Couto <weareockham@gmail.com>
2026-06-28 19:02:18 +0200

    mosaico

--- pages/components/DailyGrid.jsx
+++ pages/components/DailyGrid.jsx
@@ -2,16 +2,16 @@ import Image from "next/image";
 
 const layouts = [
   // YOUR ORIGINAL BASE (untouched)
-  { span: 3, start: 1, y: 20 },     
+  { span: 3, start: 1, y: 50 },     
   { span: 2, start: 5, y: 340 },    
-  { span: 3, start: 7, y: 20 },     
+  { span: 3, start: 7, y: 50 },     
   { span: 2, start: 11, y: 120 },   
   { span: 2, start: 2, y: 60 },     
   { span: 3, start: 6, y: 180 },    
   { span: 2, start: 10, y: 100 },
 
   // NEW BLOCK (8–14)
-  { span: 3, start: 1, y: 100 },
+  { span: 3, start: 1, y: 80 },
   { span: 2, start: 4, y: 420 },
   { span: 3, start: 7, y: 180 },
   { span: 2, start: 11, y: 240 },
@@ -22,8 +22,7 @@ const layouts = [
   // NEW BLOCK (15–21)
   { span: 3, start: 1, y: 280 },
   { span: 2, start: 5, y: 520 },
-  { span: 3, start: 7, y: 260 },
-  { span: 2, start: 11, y: 340 },
+  { span: 3, start: 9, y: 260 },
   { span: 2, start: 2, y: 320 },
   { span: 3, start: 6, y: 420 },
   { span: 2, start: 10, y: 360 },
