b2dc0bdba40a6ad09b418cfe37e976e8afc2ef35
Lucas Couto <weareockham@gmail.com>
2026-06-28 18:48:18 +0200

    mosaico

--- pages/components/DailyGrid.jsx
+++ pages/components/DailyGrid.jsx
@@ -1,42 +1,37 @@
 import Image from "next/image";
 
 const layouts = [
-  // ROW 1
-  { span: 3, start: 1, y: 20 },    
-  { span: 2, start: 5, y: 260 },   
-  { span: 3, start: 7, y: 40 },    
-  { span: 2, start: 11, y: 140 },  
+  // YOUR ORIGINAL BASE (untouched)
+  { span: 3, start: 1, y: 20 },     
+  { span: 2, start: 5, y: 340 },    
+  { span: 3, start: 7, y: 20 },     
+  { span: 2, start: 11, y: 120 },   
+  { span: 2, start: 2, y: 60 },     
+  { span: 3, start: 6, y: 180 },    
+  { span: 2, start: 10, y: 100 },
 
-  // ROW 2
-  { span: 2, start: 2, y: 100 },   
-  { span: 4, start: 6, y: 260 },   // HERO
-  { span: 2, start: 10, y: 160 },  
+  // NEW BLOCK (8–14)
+  { span: 3, start: 1, y: 160 },
+  { span: 2, start: 5, y: 420 },
+  { span: 3, start: 7, y: 180 },
+  { span: 2, start: 11, y: 240 },
+  { span: 2, start: 2, y: 220 },
+  { span: 3, start: 6, y: 340 },
+  { span: 2, start: 10, y: 260 },
 
-  // ROW 3
-  { span: 3, start: 1, y: 140 },   
-  { span: 2, start: 5, y: 280 },   
-  { span: 3, start: 8, y: 180 },   
-  { span: 2, start: 11, y: 100 },  
-
-  // ROW 4
-  { span: 2, start: 2, y: 180 },   
-  { span: 4, start: 5, y: 340 },   // HERO
-  { span: 3, start: 9, y: 200 },   
-
-  // ROW 5
-  { span: 3, start: 1, y: 160 },   
-  { span: 2, start: 5, y: 300 },   
-  { span: 3, start: 7, y: 220 },   
-  { span: 2, start: 11, y: 120 },  
-
-  // LAST
-  { span: 3, start: 3, y: 220 },   
-  { span: 4, start: 7, y: 380 },   
+  // NEW BLOCK (15–21)
+  { span: 3, start: 1, y: 280 },
+  { span: 2, start: 5, y: 520 },
+  { span: 3, start: 7, y: 260 },
+  { span: 2, start: 11, y: 340 },
+  { span: 2, start: 2, y: 320 },
+  { span: 3, start: 6, y: 420 },
+  { span: 2, start: 10, y: 360 },
 ];
 
 const DailyGrid = ({ images }) => {
   return (
-    <div className="grid grid-cols-12 px-4 gap-y-28 pt-8 pb-32">
+    <div className="grid grid-cols-12 px-4 gap-y-16 pt-8 pb-32">
       {images?.map((img, i) => {
         const layout = layouts[i];
 
@@ -45,7 +40,7 @@ const DailyGrid = ({ images }) => {
         return (
           <div
             key={i}
-            className="col-span-12"
+            className="relative"
             style={{
               gridColumn: `${layout.start} / span ${layout.span}`,
               transform: `translateY(${layout.y}px)`,
--- pages/daily.jsx
+++ pages/daily.jsx
@@ -2,15 +2,28 @@ import DailyGrid from "./components/DailyGrid";
 
 export default function Daily() {
   const mockImages = [
-    "/img/daily/1.jpg",
-    "/img/daily/2.jpg",
-    "/img/daily/3.jpg",
-    "/img/daily/4.jpg",
-    "/img/daily/5.jpg",
-    "/img/daily/6.jpg",
-    "/img/daily/7.jpg",
-    "/img/daily/8.jpg",
-  ];
+  "/img/daily/1.jpg",
+  "/img/daily/2.jpg",
+  "/img/daily/3.jpg",
+  "/img/daily/4.jpg",
+  "/img/daily/5.jpg",
+  "/img/daily/6.jpg",
+  "/img/daily/7.jpg",
+  "/img/daily/8.jpg",
+  "/img/daily/9.jpg",
+  "/img/daily/10.jpg",
+  "/img/daily/11.jpg",
+  "/img/daily/12.jpg",
+  "/img/daily/13.jpg",
+  "/img/daily/14.jpg",
+  "/img/daily/15.jpg",
+  "/img/daily/16.jpg",
+  "/img/daily/17.jpg",
+  "/img/daily/18.jpg",
+  "/img/daily/19.jpg",
+  "/img/daily/20.jpg",
+  "/img/daily/21.jpg",
+];
 
   return (
     <div className="bg-white min-h-screen">
