892a4d9c0173b7b464f961ebccffa99b27b1e3c0
Lucas Couto <weareockham@gmail.com>
2026-06-28 20:19:38 +0200

    fix header home link from daily

--- pages/components/Header.jsx
+++ pages/components/Header.jsx
@@ -31,19 +31,17 @@ const Header = ({ setFullscreen }) => {
       <header className="flex justify-between fixed z-[70] top-0 w-full px-6 py-4 mix-blend-difference bg-black text-white pointer-events-none">
         {/* Left side */}
         <div
-          className={`text-[13px] md:text-[17px] ${
-            isAbout ? "pointer-events-none opacity-0" : ""
-          }`}
-          onClick={() => setFullscreen?.(false)}
-        >
-          {!isAbout && (
-            <Link href="/">
-              <>
-                We’re <span className="font-bold">OCKHAM</span>, a directing duo
-              </>
-            </Link>
-          )}
-        </div>
+  className={`pointer-events-auto text-[13px] md:text-[17px] ${
+    isAbout ? "pointer-events-none opacity-0" : ""
+  }`}
+  onClick={() => setFullscreen?.(false)}
+>
+  {!isAbout && (
+    <Link href="/" className="cursor-pointer">
+      We’re <span className="font-bold">OCKHAM</span>, a directing duo
+    </Link>
+  )}
+</div>
 
         {/* Right side */}
         <nav className="flex text-[13px] tracking-tight pointer-events-auto">
