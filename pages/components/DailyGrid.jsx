fa4055a751a71b364864fd828dfd9c0faa1b01ef
Lucas Couto <weareockham@gmail.com>
2026-06-28 13:36:42 +0200

    add missing daily images

--- pages/components/DailyGrid.jsx
+++ pages/components/DailyGrid.jsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 
 const layouts = [
-  { span: 3, start: 1, y: 0 },
-  { span: 2, start: 5, y: 60 },
-  { span: 3, start: 7, y: 20 },
-  { span: 2, start: 11, y: 100 },
-  { span: 2, start: 2, y: 40 },
-  { span: 3, start: 6, y: 140 },
-  { span: 2, start: 10, y: 80 },
+  { span: 3, start: 1, y: 0 },      // izquierda arriba
+  { span: 2, start: 5, y: 120 },    // más abajo
+  { span: 4, start: 7, y: 40 },     // más grande y más separada
+  { span: 2, start: 11, y: 120 },   // derecha arriba
+
+  { span: 2, start: 2, y: 60 },
+  { span: 3, start: 6, y: 220 },
+  { span: 2, start: 10, y: 140 },
 ];
 
 const DailyGrid = ({ images }) => {
