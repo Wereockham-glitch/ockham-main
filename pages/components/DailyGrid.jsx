2533e1a1f26ae776517967ce753fa2738665f43c
Lucas Couto <weareockham@gmail.com>
2026-06-28 19:34:27 +0200

    mosaico

--- pages/components/DailyGrid.jsx
+++ pages/components/DailyGrid.jsx
@@ -21,9 +21,9 @@ const layouts = [
 
   // NEW BLOCK (15–21)
   { span: 3, start: 1, y: 280 },
-  { span: 2, start: 5, y: 520 },
-  { span: 3, start: 9, y: 260 },
-  { span: 2, start: 11, y: 340 },
+  { span: 2, start: 6, y: 520 },
+  { span: 3, start: 10, y: 260 },
+  { span: 2, start: 8, y: 340 },
   { span: 2, start: 2, y: 320 },
   { span: 3, start: 6, y: 420 },
   { span: 2, start: 10, y: 360 },
